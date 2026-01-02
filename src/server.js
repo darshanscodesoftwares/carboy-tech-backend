89dbbafec7a9bec3caf2fdd17660992909d4eb1e
darshanscodesoftwares <darshan.scodesoftwares@gmail.com>
2026-01-02 14:19:17 +0530

    update: port change

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { connectDatabase } = require('./config/database');
 const app = require('./app');
 
-const PORT = 5000;
+const PORT = 5001;
 
 async function start() {
   await connectDatabase();  // <--- THIS MUST RUN BEFORE app.listen()
